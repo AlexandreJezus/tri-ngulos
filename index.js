c984faa8dfe1ef3048542635c96ca9cab76cba93
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-27 09:43:18 -0300

    feat triangulos

---
+++ index.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+let ladoA = +prompt("Digite o lado A do triângulo.");
+let ladoB = +prompt("Digite o lado B do triângulo.");
+let ladoC = +prompt("Digite o lado C do triângulo.");
+
+if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC) {
+  console.log("O triângulo é equilátero.");
+} else if (
+  (ladoA !== ladoB && ladoA === ladoC) ||
+  (ladoA === ladoB && ladoA !== ladoC) ||
+  (ladoA !== ladoB && ladoA !== ladoC && ladoB === ladoC)
+) {
+  console.log("O triângulo é isósceles");
+} else {
+  console.log("O triângulo é escaleno.");
+}
