c75ad49ab054d80be5cf0c1925934f13bc95621c
Alexandre Jezus <alexandrejezus.p@gmail.com>
2024-06-27 10:35:54 -0300

    feat

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ let ladoA = +prompt("Digite o lado A do triângulo.");
 let ladoB = +prompt("Digite o lado B do triângulo.");
 let ladoC = +prompt("Digite o lado C do triângulo.");
 
-if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC) {
-  console.log("O triângulo é equilátero.");
-} else if (
-  (ladoA !== ladoB && ladoA === ladoC) ||
-  (ladoA === ladoB && ladoA !== ladoC) ||
-  (ladoA !== ladoB && ladoA !== ladoC && ladoB === ladoC)
-) {
-  console.log("O triângulo é isósceles");
-} else {
-  console.log("O triângulo é escaleno.");
+if (ladoA > 0 && ladoB > 0 && ladoC > 0) {
+  if (ladoA === ladoB && ladoA === ladoC && ladoB === ladoC) {
+    console.log("O triângulo é equilátero.");
+  } else if (
+    (ladoA !== ladoB && ladoA === ladoC) ||
+    (ladoA === ladoB && ladoA !== ladoC) ||
+    (ladoA !== ladoB && ladoA !== ladoC && ladoB === ladoC)
+  ) {
+    console.log("O triângulo é isósceles");
+  } else {
+    console.log("O triângulo é escaleno.");
+  }
 }
